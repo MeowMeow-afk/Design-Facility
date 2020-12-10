70017ed5e4eac10d025571fae0894e4116e1c565
yashchcuhan <forcourse1234@gmail.com>
2020-12-10 21:24:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import DesignFacility from "./components/DesignFacility/DesignFacility";
 
+import QuestionCheck from "./components/QuestionCheck/QuestionCheck";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    async function fetchdata() {
+      await axios
+        .get(
+          "http://5.181.217.46/DesignFacility/useGETMethodForTheResponse/yashchauhan"
+        )
+        .then((response) => {
+          // console.log(
+          //   response?.data?.exam?.sections[0].Section1[0],
+          //   typeof response?.data?.exam?.sections[0]
+          // );
+          let x = response?.data?.exam?.sections;
+          // console.log(x);
+          setData(x);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    fetchdata();
+  }, []);
+  // console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container__1">
+        <DesignFacility section={data} />
+      </div>
+      <div className="container__2">
+        <QuestionCheck />
+      </div>
     </div>
   );
 }
